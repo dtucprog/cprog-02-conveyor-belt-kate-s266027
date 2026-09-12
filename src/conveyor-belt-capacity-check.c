a173428a5d4b4cf676b9e99aa0147ae8c9ee0a22
Kate N Høyer <s266027@student.dtu.dk>
2026-09-12 22:22:40 +0200

    Update motor count and package weight in checks

--- src/conveyor-belt-capacity-check.c
+++ src/conveyor-belt-capacity-check.c
@@ -7,14 +7,14 @@ int main() {
 
     printf ("How many motors are carrying the packages?\n") ; 
     scanf("%d", &motorCount) ; 
-    motorCount = 2 ; 
-printf("%d\n", 2) ;
+    motorCount = 3 ; 
+printf("%d\n", 3) ;
 
 
     printf ("How many kg of packages do we expect? 10\n") ;
     scanf("%d", &totalPackageWeight) ;
-    totalPackageWeight = 26 ;
-printf("%d\n", 26) ;
+    totalPackageWeight = 10 ;
+printf("%d\n", 10) ;
   
  
 
